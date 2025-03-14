8df48873917a0bee493d99ff947269f50d4eec4b
nunininu <sgcho0907@gmail.com>
2025-03-14 12:48:41 +0900

    test ing

---
+++ tests/test_call.py
@@ -0,0 +1,20 @@
+from movie.api.call import gen_url, call_api
+import os
+
+def test_gen_url_default():
+    r = gen_url()
+    print(r)
+    assert "kobis" in r
+    assert "targetDt" in r
+    assert os.getenv("MOVIE_KEY") in r
+    
+def test_gen_url_default():
+    r = gen_url(url_param={"multiMovieYn": "Y", "repNationCd": "K"})
+    assert "&multiMovieYn=Y" in r
+    assert "&repNationCd=K" in r
+    
+def test_call_api():
+    r = call_api()
+    assert isinstance(r, list)
+    assert isinstance(r[0]['rnum'], str)
+    assert len(list) == 10
