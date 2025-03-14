60855fa149bb6beeb1fe946108fc5ab6313900f1
nunininu <sgcho0907@gmail.com>
2025-03-14 17:38:50 +0900

    partition_cols

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import pandas as pd
 
 BASE_URL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
 KEY=os.getenv("MOVIE_KEY")
@@ -12,5 +14,37 @@ def gen_url(dt="20120101", url_param={}):
         
     return url
 
-def call_api():
-    return []
+# 강사님 코드
+def call_api(dt="20120101", url_param={}):
+    url = gen_url(dt, url_param)
+    data = requests.get(url)
+    j = data.json()
+    return j['boxOfficeResult']['dailyBoxOfficeList']
+
+# # 내가 쓴 코드
+# def call_api():
+#     response = requests.get(gen_url())
+#     data = response.json()
+#     return data['boxOfficeResult']['dailyBoxOfficeList']
+
+
+def list2df(data: list, dt: str):
+    df = pd.DataFrame(data)
+    df['dt'] = dt
+    return df
+
+# def list2df(data, ymd):
+#     import pandas as pd
+#     df = pd.DataFrame(data)
+#     df["dt"] = ymd
+#     return df
+
+def save_df(df, base_path):
+    df.to_parquet(base_path, partition_cols=['dt'])
+    save_path = f"{base_path}/dt={df['dt'][0]}"
+    return save_path
+    # ymd = "20210101"
+    # data = call_api(dt=ymd)
+    # df = list2df(data, ymd)
+    # base_path = "~/temp/movie"
+    
